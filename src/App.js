2883668b1e7b9ac0eefae42726e45df1b657a221
Wasim <waseem_wmgh@yahoo.com>
2022-12-01 08:23:09 +0100

    invoices

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function App() {
               <ProtectedRoute>
                 {/* <Invoices
                 /> */}
-                // readDataFromInvoiceComponent={readDataFromInvoiceComponent}
+                loading...{" "}
               </ProtectedRoute>
             }
           />
